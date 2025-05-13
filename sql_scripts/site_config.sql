b19ccf0d0c3be27a4f3de2fb7fac1d786763eb82
alexkwitko <60580165+alexkwitko@users.noreply.github.com>
2025-05-13 14:35:27 -0500

    Copy sql_scripts/site_config.sql from HOG_Website

---
+++ sql_scripts/site_config.sql
@@ -0,0 +1,11 @@
+-- SQL Script for site_config
+-- Generated on 2025-05-09T02:55:05.439Z
+
+-- Clear existing data
+DELETE FROM site_config;
+
+-- Insert data
+INSERT INTO site_config (id, key, value, created_at, updated_at, description) VALUES ('06aecc90-a4d6-45a2-8765-4c2b99ec92e4', 'methodology_columns_layout', '4', '2025-05-09T00:49:08.452701+00:00', '2025-05-09T01:01:40.371469+00:00', NULL);
+INSERT INTO site_config (id, key, value, created_at, updated_at, description) VALUES ('82ff93e6-28fa-4ee1-b7b5-c507e2ac2c9f', 'why_choose_heading', 'Why Choose House of Grappling', '2025-05-07T00:09:50.124096+00:00', '2025-05-09T02:43:27.914421+00:00', NULL);
+INSERT INTO site_config (id, key, value, created_at, updated_at, description) VALUES ('98eb65c4-ef02-4029-bd2f-bfd4551090cd', 'why_choose_subheading', 'We offer a world-class training environment focused on technical excellence, personal growth, and community.', '2025-05-07T00:09:50.124096+00:00', '2025-05-09T02:43:28.020275+00:00', NULL);
+INSERT INTO site_config (id, key, value, created_at, updated_at, description) VALUES ('abb7cea0-7d46-4eea-aca3-8f16960e4237', 'why_choose_columns_layout', '2', '2025-05-09T00:46:26.873435+00:00', '2025-05-09T02:43:28.131997+00:00', NULL);
