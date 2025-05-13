e4662e73a687c0f39cb9327b63a383a8023175e5
alexkwitko <60580165+alexkwitko@users.noreply.github.com>
2025-05-13 14:37:53 -0500

    Copy supabase.ts from HOG_Website

---
+++ supabase.ts
@@ -0,0 +1,9 @@
+[?25l
+    Select a project:                                                                        
+                                                                                             
+  >  1. ucksziiwerwqqjllnekn [name: BJJ-BOOST, org: piskbqzawmppqhnqqlgw, region: us-east-1] 
+    2. yxwwmjubpkyzwmvilmsw [name: HOG-Website, org: piskbqzawmppqhnqqlgw, region: us-east-2]
+                                                                                             
+                                                                                             
+    ↑/k up • ↓/j down • / filter • q quit • ? more                                           
+                                                                                             [0D
